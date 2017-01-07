09b6077da27a5147ed16bb0a8e4dd534e4bc3d47
Scott Boyle <scott@monospaced.com>
2017-01-07 21:01:50 +0000

    Consistent route declarations

--- scripts/entry.js
+++ scripts/entry.js
@@ -3,18 +3,18 @@ import ReactDOM from'react-dom';
 import ReactDOMServer from'react-dom/server';
 import {Router, browserHistory, createMemoryHistory} from 'react-router';
 import DocumentMeta from 'react-document-meta';
-import Routes from './routes';
 import Root from '../components/Root/Root.js';
+import routes from './routes';
 
 if (typeof document !== 'undefined') {
   const app = document.getElementById('app');
-  ReactDOM.render(<Router history={browserHistory} routes={Routes}/>, app);
+  ReactDOM.render(<Router history={browserHistory} routes={routes}/>, app);
 }
 
 export default (locals, callback) => {
   const history = createMemoryHistory(locals.path);
-  const app = {__html: ReactDOMServer.renderToString(<Router history={history}>{Routes}</Router>)};
+  const app = {__html: ReactDOMServer.renderToString(<Router history={history} routes={routes}/>)};
   const meta = DocumentMeta.renderAsReact();
-  const html = ReactDOMServer.renderToStaticMarkup(<Root meta={meta} app={app} />);
+  const html = ReactDOMServer.renderToStaticMarkup(<Root meta={meta} app={app}/>);
   callback(null, '<!DOCTYPE html>' + html);
 };
